ca5ab550c88745c88d2501334f75cd784d0752a8
LucasAdorno <lucasadorno2702@gmail.com>
2020-06-25 08:05:41 -0300

    all commits

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+export default function Header() {
+  return ( 
+    <Container>
+        <h1>Adornofy</h1>
+    </Container>
+  );
+}
+
